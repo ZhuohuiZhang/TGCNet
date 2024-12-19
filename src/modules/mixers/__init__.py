1e9174dd5e3d2f2bd490d84b616ecd1bef47e0d1
ZhuohuiZhang <zhangzhuohui@tongji.edu.cn>
2024-12-19 16:12:25 +0800

    Update

--- src/modules/mixers/__init__.py
+++ src/modules/mixers/__init__.py
@@ -1,6 +1,9 @@
-# -*- coding:utf-8 -*-
-# @Time : 2024/12/19 15:45
-# @Author : Zhuohui Zhang
-# @File : __init__.py.py
-# @Project : TGCNet
-# @mail: zhangzh.grey@gmail.com
+# -*- coding: utf-8 -*-
+# @Time    : 2023/7/27 22:12
+# @Author  : Zhuohui Zhang
+# @File    : __init__.py
+# @Software: PyCharm
+# @mail    : zhangzh.grey@gmail.com
+from .graph_coarsening import GraphCoarsening
+
+REGISTRY = {"coarsen": GraphCoarsening,}
